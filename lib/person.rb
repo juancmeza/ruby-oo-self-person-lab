da36b72b17f091234506f703df555e844bed9447
juan_carlos_meza <juancmezat@gmail.com>
2020-11-18 10:39:16 -0800

    commit tree d810498cd08339c30e500899f758d8a11ad3dbd1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
 # your code goes here
+class Person
+    
+    attr_reader :name
+
+    def initialize(name, bank_account=25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(happiness_value)
+        if happiness_value > 0 && happiness_value < 11
+            return happiness_value
+        else
+            raise "Out of Bounds" 
+        end
+    end
+
+    attr_accessor :bank_account
+
+end
+
+
+    
